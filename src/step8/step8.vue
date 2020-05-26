0e36d42f522043f8a82629214ae5427b49b27e08
rakisuta_bun <raki0142@gmail.com>
2020-05-27 01:53:18 +0900

    あっち向いてほいマシーンの制作を開始

--- src/step8/step8.vue
+++ src/step8/step8.vue
@@ -1,9 +1,165 @@
 <template>
-  <h1>hogehoge</h1>
+  <div>
+    <h1>あっちむいてほいマシーン</h1>
+    <div>人間の手: {{ humanHand }}</div>
+    <h1>{{ humanpic }}</h1>
+    <div>コンピュータの手: {{ comHand }}</div>
+    <h1>{{ compic }}</h1>
+    <div>
+      <a href="#" class="btn btn-lg btn-primary" @click="onJanken('グー')">グー</a>
+      <a href="#" class="btn btn-lg btn-primary" @click="onJanken('チョキ')">チョキ</a>
+      <a href="#" class="btn btn-lg btn-primary" @click="onJanken('パー')">パー</a>
+      <a href="#" class="btn btn-lg btn-primary" @click="onJanken('グーチョキパー')" v-if="aviableGuChokiPa()">猫の手を借りる</a>
+    </div>
+    <h3>カウント: {{ jankenCount }}</h3>
+    <h3>猫エネルギー: {{ energy }}</h3>
+    <h3>勝敗: {{ result }}</h3>
+    <!-- <div v-if="aviableJyankenWin()"> -->
+    <div>
+      <a href="#" class="btn btn-lg btn-primary" @click="onAttimuite('ウエ')">↑</a>
+      <a href="#" class="btn btn-lg btn-primary" @click="onAttimuite('シタ')">↓</a>
+      <a href="#" class="btn btn-lg btn-primary" @click="onAttimuite('ミギ')">→</a>
+      <a href="#" class="btn btn-lg btn-primary" @click="onAttimuite('ヒダリ')">←</a>
+      <div>人間の向き: {{ humanMuki }}</div>
+      <div>コンピュータの向き: {{ comMuki }}</div>
+      <h3>勝敗: {{ hoiResult }}</h3>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+export default {
+  data() {
+    return {
+      humanHand: "",
+      comHand: "",
+      humanpic: "",
+      compic: "",
+      result: "",
+      jankenCount: 0,
+      energy: "🐱：☆☆☆",
+      humanMuki: "",
+      comMuki: "",
+      hoiResult: "",
+    };
+  },
+  methods: {
+    onJanken(hand) {
+      this.humanHand = hand;
+      this.humanpic = this.emoji(this.humanHand);
+      this.comHand = this.getComHand();
+      this.compic = this.emoji(this.comHand);
+      this.result = this.hantei();
+      this.jankenCount++;
+      this.reset = this.cooltime();
+      this.energy = this.nekocount();
+    },
+    onAttimuite(muki) {
+      this.humanMuki = muki;
+      this.comMuki = this.getComMuki();
+      this.hoiResult = this.hoiHantei();
+    },
+    getComMuki() {
+      const mukis = ["ウエ", "シタ", "ミギ", "ヒダリ"];
+      const muki = mukis[Math.floor(Math.random() * mukis.length)];
+      return muki;
+    },
+    hoiHantei() {
+      const win = "あなたの勝ち！";
+      const lose = "あなたの負け！";
+      const mokkai = "もう一回！";
 
-<style></style>
+      if (this.humanMuki == this.comMuki) {
+        return win;
+      }
+      if (this.comMuki == this.humanMuki) {
+        return lose;
+      }
+      return mokkai;
+    },
+    // aviableJyankenWin() {
+    //   if ((this.hantei() = win)) {
+    //     return true;
+    //   }
+    //   return false;
+    // },
+    nekocount() {
+      if (this.jankenCount == 0) {
+        return "🐱：☆☆☆";
+      }
+      if (this.jankenCount == 1) {
+        return "🐱：★☆☆";
+      }
+      if (this.jankenCount == 2) {
+        return "🐱：★★☆";
+      }
+      if (this.jankenCount >= 3) {
+        return "🐱：★★★[!!!CAT POWER MAX!!!]";
+      }
+    },
+    emoji(hand) {
+      if (hand == "グー") {
+        return "✊";
+      }
+      if (hand == "チョキ") {
+        return "✌";
+      }
+      if (hand == "パー") {
+        return "✋";
+      }
+      if (hand == "グーチョキパー") {
+        return "🐱";
+      }
+      return "💀";
+    },
+    getComHand() {
+      const hands = ["グー", "チョキ", "パー"];
+      const hand = hands[Math.floor(Math.random() * hands.length)];
+      return hand;
+    },
+    aviableGuChokiPa() {
+      if (this.jankenCount >= 3) {
+        return true;
+      }
+      return false;
+    },
+    cooltime() {
+      if (this.humanHand == "グーチョキパー") {
+        this.jankenCount = 0;
+      }
+    },
+
+    hantei() {
+      const win = "あなたの勝ち！";
+      const lose = "コンピュータの勝ち！";
+      const draw = "あいこ！";
+
+      if (this.humanHand == "グー" && this.comHand == "チョキ") {
+        return win;
+      }
+      if (this.humanHand == "チョキ" && this.comHand == "パー") {
+        return win;
+      }
+      if (this.humanHand == "パー" && this.comHand == "グー") {
+        return win;
+      }
+      if (this.humanHand == "グー" && this.comHand == "パー") {
+        return lose;
+      }
+      if (this.humanHand == "チョキ" && this.comHand == "グー") {
+        return lose;
+      }
+      if (this.humanHand == "パー" && this.comHand == "チョキ") {
+        return lose;
+      }
+      if (this.humanHand == this.comHand) {
+        return draw;
+      }
+      if (this.humanHand == "グーチョキパー") {
+        return win;
+      }
+      return lose;
+    },
+  },
+};
+</script>
