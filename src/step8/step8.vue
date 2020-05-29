1115d665b72189861442fd0672065861b34b8653
rakisuta_bun <raki0142@gmail.com>
2020-05-29 14:05:25 +0900

    方向が違ったときにもちゃんとじゃんけんステップに戻るようにした

--- src/step8/step8.vue
+++ src/step8/step8.vue
@@ -39,6 +39,7 @@
           <a href="#" class="btn btn-lg btn-primary" @click="onJanken('パー')">パー</a>
           <a href="#" class="btn btn-lg btn-primary" @click="onJanken('グーチョキパー')" v-if="aviableGuChokiPa()">猫の手を借りる</a>
           <h3>猫エネルギー: {{ energy }}</h3>
+          <h3>{{ hoiResult }}</h3>
         </div>
         <!-- <h3>カウント: {{ jankenCount }}</h3> -->
         <div v-if="step == 2" class="col-md-6">
@@ -139,6 +140,7 @@ export default {
         return lose;
       }
       this.winStatus = 3;
+      this.step = 1;
       return mokkai;
     },
     nekocount() {
