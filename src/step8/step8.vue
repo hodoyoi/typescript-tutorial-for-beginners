79b5fa2a7f965416cec4d304c24e3203c8289760
rakisuta_bun <raki0142@gmail.com>
2020-05-30 21:10:06 +0900

    スタート画面のズレ修正

--- src/step8/step8.vue
+++ src/step8/step8.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="container">
-    <h1>あっちむいてほいマシーン</h1>
     <div v-if="step == 0">
       <div class="row">
-        <img class="mx-auto" src="/static/start.png" />
+        <img class="mx-auto img-fluid" src="/static/start.png" />
       </div>
       <div class="row">
         <a href="#" class="btn btn-lg btn-primary mx-auto" @click="onStart()">すたーと</a>
@@ -11,7 +10,7 @@
     </div>
     <div v-if="step == 1 || step == 2">
       <div class="row">
-        <div class="col-md-3">
+        <div class="col-6 col-md-3">
           <div class="card">
             <img src="/static/human.png" class="img card-img-top" />
             <div class="card-body">
@@ -20,7 +19,7 @@
             </div>
           </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-6 col-md-3">
           <div class="card">
             <img src="/static/cpu.png" class="img card-img-top" />
             <div class="card-body">
@@ -29,7 +28,7 @@
             </div>
           </div>
         </div>
-        <div v-if="step == 1" class="col-md-6">
+        <div v-if="step == 1" class="col-12 col-md-6">
           <h3>じゃんけん: {{ result }}</h3>
           <a href="#" class="btn btn-lg btn-primary" @click="onJanken('グー')">グー</a>
           <a href="#" class="btn btn-lg btn-primary" @click="onJanken('チョキ')">チョキ</a>
